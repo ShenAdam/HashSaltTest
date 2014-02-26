d6afcc1e43af4d9ee55ac0005026156f4981b0ed
ShenAdam <Adam.Brill@emp.shentel.com>
2014-02-26 10:24:12 -0500

    Cleanup and new testset
    added a new test set for PasswordHash and cleaned up little bit, still a
    mess though, but who cares i'm just playing around

--- ConsoleApplication2/ConsoleApplication2/Program.cs
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -1,153 +1,16 @@
 ﻿using System;
-using System.IO;
-using System.Security.Cryptography;
 using System.Text;
-using System.Threading;
 using System.Web;
-using System;
-using System.Globalization;
-
-
-public class Crypto
-{
-    private static byte[] _salt = Encoding.ASCII.GetBytes("olaskeois3jdfluIlulJsss3shent323lskj");
-
-    /// <summary>
-    /// Encrypt the given string using AES.  The string can be decrypted using 
-    /// DecryptStringAES().  The sharedSecret parameters must match.
-    /// </summary>
-    /// <param name="plainText">The text to encrypt.</param>
-    /// <param name="sharedSecret">A password used to generate a key for encryption.</param>
-    public static string EncryptStringAES(string plainText, string sharedSecret)
-    {
-        if (string.IsNullOrEmpty(plainText))
-            throw new ArgumentNullException("plainText");
-        if (string.IsNullOrEmpty(sharedSecret))
-            throw new ArgumentNullException("sharedSecret");
-
-        string outStr = null;                       // Encrypted string to return
-        RijndaelManaged aesAlg = null;              // RijndaelManaged object used to encrypt the data.
-
-        try
-        {
-            // generate the key from the shared secret and the salt
-            Rfc2898DeriveBytes key = new Rfc2898DeriveBytes(sharedSecret, _salt);
-
-            // Create a RijndaelManaged object
-            aesAlg = new RijndaelManaged();
-            aesAlg.Key = key.GetBytes(aesAlg.KeySize / 8);
-
-            // Create a decryptor to perform the stream transform.
-            ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
-
-            // Create the streams used for encryption.
-            using (MemoryStream msEncrypt = new MemoryStream())
-            {
-                // prepend the IV
-                msEncrypt.Write(BitConverter.GetBytes(aesAlg.IV.Length), 0, sizeof(int));
-                msEncrypt.Write(aesAlg.IV, 0, aesAlg.IV.Length);
-                using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
-                {
-                    using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
-                    {
-                        //Write all data to the stream.
-                        swEncrypt.Write(plainText);
-                    }
-                }
-                outStr = Convert.ToBase64String(msEncrypt.ToArray());
-            }
-        }
-        finally
-        {
-            // Clear the RijndaelManaged object.
-            if (aesAlg != null)
-                aesAlg.Clear();
-        }
-
-        // Return the encrypted bytes from the memory stream.
-        return outStr;
-    }
-
-    /// <summary>
-    /// Decrypt the given string.  Assumes the string was encrypted using 
-    /// EncryptStringAES(), using an identical sharedSecret.
-    /// </summary>
-    /// <param name="cipherText">The text to decrypt.</param>
-    /// <param name="sharedSecret">A password used to generate a key for decryption.</param>
-    public static string DecryptStringAES(string cipherText, string sharedSecret)
-    {
-        if (string.IsNullOrEmpty(cipherText))
-            throw new ArgumentNullException("cipherText");
-        if (string.IsNullOrEmpty(sharedSecret))
-            throw new ArgumentNullException("sharedSecret");
-
-        // Declare the RijndaelManaged object
-        // used to decrypt the data.
-        RijndaelManaged aesAlg = null;
-
-        // Declare the string used to hold
-        // the decrypted text.
-        string plaintext = null;
-
-        try
-        {
-            // generate the key from the shared secret and the salt
-            Rfc2898DeriveBytes key = new Rfc2898DeriveBytes(sharedSecret, _salt);
-
-            // Create the streams used for decryption.                
-            byte[] bytes = Convert.FromBase64String(cipherText);
-            using (MemoryStream msDecrypt = new MemoryStream(bytes))
-            {
-                // Create a RijndaelManaged object
-                // with the specified key and IV.
-                aesAlg = new RijndaelManaged();
-                aesAlg.Key = key.GetBytes(aesAlg.KeySize / 8);
-                // Get the initialization vector from the encrypted stream
-                aesAlg.IV = ReadByteArray(msDecrypt);
-                // Create a decrytor to perform the stream transform.
-                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
-                using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
-                {
-                    using (StreamReader srDecrypt = new StreamReader(csDecrypt))
-
-                        // Read the decrypted bytes from the decrypting stream
-                        // and place them in a string.
-                        plaintext = srDecrypt.ReadToEnd();
-                }
-            }
-        }
-        finally
-        {
-            // Clear the RijndaelManaged object.
-            if (aesAlg != null)
-                aesAlg.Clear();
-        }
-
-        return plaintext;
-    }
-
-    private static byte[] ReadByteArray(Stream s)
-    {
-        byte[] rawLength = new byte[sizeof(int)];
-        if (s.Read(rawLength, 0, rawLength.Length) != rawLength.Length)
-        {
-            throw new SystemException("Stream did not contain properly formatted byte array");
-        }
-
-        byte[] buffer = new byte[BitConverter.ToInt32(rawLength, 0)];
-        if (s.Read(buffer, 0, buffer.Length) != buffer.Length)
-        {
-            throw new SystemException("Did not read byte array properly");
-        }
-
-        return buffer;
-    }
-}
-
 
 public class Program
 {
-    private static Random random = new Random((int)DateTime.Now.Ticks);//thanks to McAden
+    private static Random random = new Random((int)DateTime.Now.Ticks);
+
+    /// <summary>
+    /// Makes random strings for testing
+    /// </summary>
+    /// <param name="size">size of string</param>
+    /// <returns>the random string</returns>
     public static string RandomString(int size)
     {
         StringBuilder builder = new StringBuilder();
@@ -161,54 +24,74 @@ public class Program
         return builder.ToString();
     }
 
+    /// <summary>
+    /// This is the test section for Crypto
+    /// </summary>
+    public static void Main()
+    {
+        string[] strArray = new string[100];
+        strArray[0] = "-8a5asdz88=11asdfasdfas3a+sdf62sasfdd332f25829+1223sasdzf657570sd2";
+        strArray[1] = "lsdjfsDKFSJDF323323lk4ssdfjlSJDF32l324klj32SDSF32dwS33DdSds34320893";
+        strArray[2] = "JKALSDLFAKLSFLDFLlskadjfsalkdjflksajfdfl3242320934028349823409240230";
+        strArray[3] = "309284jklsadfAJSDFLXKLJVC324sdf234ASDF324sdfsdlkjew32dswasdfasdfaAAA";
 
-public static void Main()
-   {
-      // for (int i = 0; i < 20; i++)
-      // {
-      //     var x = Program.RandomString(10);
-      //    System.Console.WriteLine(x);
-      //    x = Crypto.EncryptStringAES(x, "temp");
-      //    System.Console.WriteLine(x);
-      //    x = Crypto.DecryptStringAES(x, "temp");
-      //    System.Console.WriteLine(x);
-      //}
+        for (int i = 4; i < 100; i++)
+        {
+            strArray[i] = Program.RandomString(100);
+        }
 
-      string[] x = new string[1000];
-      x[0] = "-8a5asdz88=11asdfasdfas3a+sdf62sasfdd332f25829+1223sasdzf657570sd2";
-      x[1] = "lsdjfsDKFSJDF323323lk4ssdfjlSJDF32l324klj32SDSF32dwS33DdSds34320893";
-      x[2] = "JKALSDLFAKLSFLDFLlskadjfsalkdjflksajfdfl3242320934028349823409240230";
-      x[3] = "309284jklsadfAJSDFLXKLJVC324sdf234ASDF324sdfsdlkjew32dswasdfasdfaAAA";
+        var falseCount = 0;
+        foreach (var item in strArray)
+        {
+            //System.Console.WriteLine(item);
+            var encrypted = Crypto.EncryptStringAES(item, "EAAAAHBXawUX8ib6pJF0gea3czder4x9KWbLMxDx7AnKFkyG4gUp9D+XzhcemWXnkPLv/3VCCo4oCzFZR6p+mTocJq3tEBz7LNh7sT9uOW+J9XUD");
+            //System.Console.WriteLine(encrypted);
 
-      for (int i = 4; i < 1000; i++)
-      {
-          x[i] = Program.RandomString(100);
-      }
+            var EncodedURL = HttpUtility.UrlEncode(encrypted);
+            var DecodedURL = HttpUtility.UrlDecode(encrypted);
+            //System.Console.WriteLine("Encoded URL:"+EncodedURL);
+            bool match = DecodedURL.Replace(" ", "+") == encrypted;
+            if (match)
+                falseCount += 1;
+            //System.Console.WriteLine(match.ToString());
+            //System.Console.WriteLine(DecodedURL);
+            //System.Console.WriteLine(encrypted);
+            //System.Console.WriteLine("-----------");
 
-      var falseCount = 0;
-      foreach (var item in x)
-      {
-          //System.Console.WriteLine(item);
-          var encrypted = Crypto.EncryptStringAES(item, "EAAAAHBXawUX8ib6pJF0gea3czder4x9KWbLMxDx7AnKFkyG4gUp9D+XzhcemWXnkPLv/3VCCo4oCzFZR6p+mTocJq3tEBz7LNh7sT9uOW+J9XUD");
-          //System.Console.WriteLine(encrypted);
+            var decrpyted = Crypto.DecryptStringAES(encrypted, "EAAAAHBXawUX8ib6pJF0gea3czder4x9KWbLMxDx7AnKFkyG4gUp9D+XzhcemWXnkPLv/3VCCo4oCzFZR6p+mTocJq3tEBz7LNh7sT9uOW+J9XUD");
+            //System.Console.WriteLine(decrpyted);
+        }
+        System.Console.WriteLine(falseCount);
+        System.Console.ReadLine();
+    }
 
-          var EncodedURL = HttpUtility.UrlEncode(encrypted);
-          var DecodedURL = HttpUtility.UrlDecode(encrypted);
-          //System.Console.WriteLine("Encoded URL:"+EncodedURL);
-          bool match = DecodedURL.Replace(" ","+") == encrypted;
-          if (match)
-              falseCount += 1;
-          //System.Console.WriteLine(match.ToString());
-          //System.Console.WriteLine(DecodedURL);
-          //System.Console.WriteLine(encrypted);
-          //System.Console.WriteLine("-----------");
+    /// <summary>
+    /// This is a test section for PasswordHash
+    /// </summary>
+    public static void Main1()
+    {
+        string[] strArrayHash = new string[100];
 
+        for (int i = 0; i < 100; i++)
+        {
+            strArrayHash[i] = Program.RandomString(10);
+        }
 
+        foreach (var item in strArrayHash)
+        {
+            var hashVal = PasswordHash.PasswordHash.CreateHash(item);
+            var hashVa2 = PasswordHash.PasswordHash.CreateHash(item);
+            var hashVa3 = PasswordHash.PasswordHash.CreateHash(item);
+            var CheckHash1 = PasswordHash.PasswordHash.ValidatePassword(item, hashVal);
+            var CheckHash2 = PasswordHash.PasswordHash.ValidatePassword(item, hashVa2);
+            var CheckHash3 = PasswordHash.PasswordHash.ValidatePassword(item, hashVa3);
+            Console.WriteLine("value:" + item);
+            Console.WriteLine("hash1:" + hashVal);
+            Console.WriteLine("hash2:" + hashVa2);
+            Console.WriteLine("hash3:" + hashVa3);
+            Console.WriteLine(CheckHash1.ToString() + CheckHash2 + CheckHash3);
+        }
 
-          var decrpyted = Crypto.DecryptStringAES(encrypted, "EAAAAHBXawUX8ib6pJF0gea3czder4x9KWbLMxDx7AnKFkyG4gUp9D+XzhcemWXnkPLv/3VCCo4oCzFZR6p+mTocJq3tEBz7LNh7sT9uOW+J9XUD");
-          //System.Console.WriteLine(decrpyted);
-      }
-      System.Console.WriteLine(falseCount);
-      System.Console.ReadLine();
-   }
+        System.Console.ReadLine();
+    }
 }
